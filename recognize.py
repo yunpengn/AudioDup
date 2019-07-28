b1807319f81577b53ea181d0e654758798db9a8d
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-07-28 15:56:45 +0800

    Add recognize script

---
+++ recognize.py
@@ -0,0 +1,20 @@
+from dejavu import Dejavu
+from dejavu.recognize import MicrophoneRecognizer
+
+# Database connection config.
+config = {
+	"database": {
+		"host": "localhost",
+		"user": "dejavu",
+		"passwd": "dejavu",
+		"db": "dejavu",
+	},
+	"database_type": "mysql",
+}
+
+# Creates a new instance.
+djv = Dejavu(config)
+
+# Reads 10 seconds from microphone and recognize.
+song = djv.recognize(MicrophoneRecognizer, seconds=10)
+print("Result: ", song)
