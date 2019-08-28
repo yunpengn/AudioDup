948ad56561e067569c160a99860413af9878a03e
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-08-28 20:14:10 +0800

    Fix upload endpoint

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 import uuid
+import os
 
 from flask import Flask, render_template, request
 
 # Creates a Flask app.
 app = Flask(__name__, template_folder='templates')
+
+# Defines the upload folder.
+upload_folder = "upload/"
 
 
 # Defines the health check endpoint.
@@ -14,13 +18,15 @@ def hello_world():
 # Defines the file upload endpoint.
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_video():
-    if request.method != 'POST':
+    if request.method != 'POST' or 'video_file' not in request.files:
         return render_template('upload.html')
 
     # Retrieves the uploaded file from request and saves.
     f = request.files['video_file']
-    file_name = str(uuid.uuid4())
+    file_name = os.path.join(app.root_path, upload_folder, str(uuid.uuid4()))
     f.save(file_name)
+
+    # Returns the result.
     return 'Hi!'
 
 
