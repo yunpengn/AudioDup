a3309e1c0bf9d5c23724a8d7c413d2701d27b583
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-08-28 13:25:20 +0800

    Fix recognize_mp4 interface

--- recognize.py
+++ recognize.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 
 from dejavu import Dejavu
@@ -14,6 +15,7 @@ config = {
     },
     "database_type": "mysql",
 }
+confidence_threshold = 100
 mp3_temp_file_format = "%s.mp3"
 
 # Creates a new instance.
@@ -22,11 +24,20 @@ djv = Dejavu(config)
 
 # Returns true if a potential duplicate is found for the sound track of the given MP4 file.
 def recognize_mp4(file_name):
+    # Extracts audio.
     video = AudioSegment.from_file(file_name, "mp4")
     audio_file_name = mp3_temp_file_format % uuid.uuid4()
     video.export(audio_file_name, format="mp3")
 
-    return recognize_mp3(audio_file_name)
+    # Recognizes the audio.
+    result = djv.recognize(FileRecognizer, audio_file_name)
+    os.remove(audio_file_name)
+
+    # Returns the result.
+    return_val = []
+    if result is not None and result['confidence'] > confidence_threshold:
+        return_val.append([result['song_name'], result['confidence']])
+    return return_val
 
 
 # Returns true if a potential duplicate is found for the given MP3 file.
@@ -40,3 +51,6 @@ def recognize_mp3_for_song(song_id, file_name):
     song = djv.recognize_for_song(FileRecognizer, song_id, file_name)
     print("Recognition result is %s" % song)
     return song is None
+
+
+print(recognize_mp4("data/videos/6522936861751512324.mp4"))
